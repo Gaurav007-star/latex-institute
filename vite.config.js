662b055e49da38277eab051b4b580a7e49a7ce38
Gaurav007-star <gauravsarkar1000b@gmail.com>
2026-02-28 22:35:10 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 8000,
-    strictPort: true, // ❗ prevents fallback
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
